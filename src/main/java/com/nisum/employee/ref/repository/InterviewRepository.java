6e15df28c082eff5c0e6a5e41749e5a4a62f349a
NISUM CONSULTING <NISUM CONSULTING@192.168.40.10>
2016-07-04 12:21:54 +0530

    Code refactor for interview service. repository code moved from service

--- src/main/java/com/nisum/employee/ref/repository/InterviewRepository.java
+++ src/main/java/com/nisum/employee/ref/repository/InterviewRepository.java
@@ -1,7 +1,13 @@
 package com.nisum.employee.ref.repository;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nisum.employee.ref.domain.InterviewDetails;
@@ -12,8 +18,19 @@ public class InterviewRepository{
 	@Autowired
 	private MongoOperations mongoOperation;
 	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	public void save(InterviewDetails interview) {
 		mongoOperation.save(interview);
+	}
+	
+	public List<InterviewDetails> interviewCheck(String candidateId) {
+		MongoOperations mongoOperations = (MongoOperations) mongoTemplate;
+		Query query = new Query();
+		query.addCriteria(Criteria.where("candidateId").regex(Pattern.compile(candidateId, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE)));
+		List<InterviewDetails> checkDetails = mongoOperations.find(query, InterviewDetails.class);
+		return checkDetails;
 	}
 
 }
