5193a164402717f0f779f8f84003e72c0527f7cc
nbolla <nbolla@nisum.com>
2016-07-12 20:24:16 +0530

    If the user is logged into the application first time then system is showing super(Admin) user data

--- src/main/java/com/nisum/employee/ref/service/UserService.java
+++ src/main/java/com/nisum/employee/ref/service/UserService.java
@@ -1,11 +1,13 @@
 package com.nisum.employee.ref.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nisum.employee.ref.converter.UserInfoConverter;
+import com.nisum.employee.ref.domain.UserInfo;
 import com.nisum.employee.ref.repository.UserInfoRepository;
 import com.nisum.employee.ref.view.UserInfoDTO;
 
@@ -28,7 +30,20 @@ public class UserService implements IUserService{
 	}
 	@Override
 	public List<UserInfoDTO> retrieveUserById(String userId) {
-		return userInfoConverter.convertToDTOs(userInfoRepository.retrieveUserById(userId));
+		
+		List<UserInfo> userInfos = userInfoRepository.retrieveUserById(userId);
+		
+		
+		if(userInfos.size() <= 0)
+		{
+			//User is not existed. That means new user is logged into the application.
+			//If the user is new user then we need to set default Role as "ROLE_USER"
+			UserInfo userInfo = new UserInfo();
+			userInfo.setRoles(Arrays.asList("ROLE_USER"));
+			userInfos.add(userInfo);
+		}
+		
+		return userInfoConverter.convertToDTOs(userInfos);
 	}
 	@Override
 	public List<UserInfoDTO> retrieveUserByName(String name) {
