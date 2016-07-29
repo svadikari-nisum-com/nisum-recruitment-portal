38faf4ded073d6b83f1600d5cfd4e0fb9f4e1c31
Shyam Vadikari <svadikari@nisum.com>
2016-07-28 23:35:12 -0700

    Error handling implementation

---
+++ src/main/java/com/nisum/employee/ref/common/ErrorCodes.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nisum.employee.ref.common;
+
+/**
+ * @author NISUM
+ *
+ */
+public final class ErrorCodes {
+
+	public static String NRP0001="NRP0001";
+	public static String NRP0002="NRP0002";
+}
--- src/main/java/com/nisum/employee/ref/controller/ExceptionAdviceController.java
+++ src/main/java/com/nisum/employee/ref/controller/ExceptionAdviceController.java
@@ -1,4 +1,5 @@
 package com.nisum.employee.ref.controller;
+
 import java.util.concurrent.ExecutionException;
 
 import javax.mail.MessagingException;
@@ -6,37 +7,46 @@ import javax.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nisum.employee.ref.common.ErrorCodes;
 import com.nisum.employee.ref.service.NotificationService;
 import com.nisum.employee.ref.view.BaseDTO;
-
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionAdviceController {
-	
+
 	@Autowired
 	private NotificationService notificationService;
-	
-	@ExceptionHandler({InterruptedException.class, RuntimeException.class, 
-		ExecutionException.class})
-    public ResponseEntity<BaseDTO> handleRuntimeErrors(Exception e) {
-        log.error(e.getMessage(), e);
-        notificationService.sendExcetionLog(e);
-        BaseDTO baseDTO = new BaseDTO();
-        baseDTO.addError("ERR001", e.getMessage());
-        return new ResponseEntity<BaseDTO>(baseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-	@ExceptionHandler({Exception.class, MessagingException.class})
-    public ResponseEntity<BaseDTO> handleSQLException(Exception e) {
-        log.error(e.getMessage(), e);
-        notificationService.sendExcetionLog(e);
-        BaseDTO baseDTO = new BaseDTO();
-        baseDTO.addError("ERR002", e.getMessage());
-        return new ResponseEntity<BaseDTO>(baseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+
+	@Autowired
+	private MessageSourceAccessor messageSourceAccessor;
+
+	@ExceptionHandler({ InterruptedException.class, RuntimeException.class,
+			ExecutionException.class })
+	public ResponseEntity<BaseDTO> handleRuntimeErrors(Exception e) {
+		log.error(e.getMessage(), e);
+		notificationService.sendExcetionLog(e);
+		BaseDTO baseDTO = new BaseDTO();
+		baseDTO.addError(ErrorCodes.NRP0001,
+				messageSourceAccessor.getMessage(ErrorCodes.NRP0001));
+		return new ResponseEntity<BaseDTO>(baseDTO,
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler({ Exception.class, MessagingException.class })
+	public ResponseEntity<BaseDTO> handleSQLException(Exception e) {
+		log.error(e.getMessage(), e);
+		notificationService.sendExcetionLog(e);
+		BaseDTO baseDTO = new BaseDTO();
+		baseDTO.addError(ErrorCodes.NRP0001,
+				messageSourceAccessor.getMessage(ErrorCodes.NRP0001));
+		return new ResponseEntity<BaseDTO>(baseDTO,
+				HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
