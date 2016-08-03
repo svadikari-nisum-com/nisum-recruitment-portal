afeaf7667d392b6f46bc0346832770f67a93533d
swatinisum <NISUM CONSULTING@192.168.40.14>
2016-08-03 16:37:04 +0530

    As a user I should be presented with dashboard events based on my role.

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -5,6 +5,7 @@ app.controller("dashboardCtrl", ['$scope', '$http', '$upload','$filter', '$timeo
 	$scope.showScheduleData = [];
 	$scope.hideNoPositionsMsg = true;
 	$scope.hideNoInterviewMsg = true;
+	$scope.useremailId = sessionStorage.userId;
 	
 	dashboardService.getPositionData()
 	.then(function(data){
@@ -18,18 +19,49 @@ app.controller("dashboardCtrl", ['$scope', '$http', '$upload','$filter', '$timeo
 	function(msg){
 		$log.error(msg);
 	});
+	var User_URL = 'resources/user?emailId='+$scope.useremailId;
 	
-	dashboardService.getScheduleData()
-	.then(function (data){
-		$scope.showScheduleData = data;
-		if(data == "" || data == null || data == undefined){
+	$http.get(User_URL).success(function(data, status, headers, config) {
+		$scope.userRoles = data[0].roles;
+		if(_.contains($scope.userRoles, "ROLE_HR") || _.contains($scope.userRoles, "ROLE_RECRUITER") || _.contains($scope.userRoles, "ROLE_ADMIN") ||  _.contains($scope.userRoles, "ROLE_MANAGER")){
+			    dashboardService.getScheduleData().then(function (data){
+				$scope.showScheduleData = data;
+					if(data == "" || data == null || data == undefined){
+						$scope.hideNoInterviewMsg = false;
+					}
+			    }).catch(function(msg){
+			 	$log.error(msg);
+				$scope.hideNoInterviewMsg = false;
+			    });
+		} else if(_.contains($scope.userRoles, "ROLE_INTERVIEWER")) {
+			$http.get('resources/getInterviewByInterviewer?interviewerEmail='+$scope.useremailId).success(function(data, status, headers, config) {
+				var showScheduleData =[];
+				var today = new Date();
+				var tomorrow = new Date(today);
+				tomorrow.setDate(today.getDate()+4);
+				angular.forEach(data, function(obj){
+					angular.forEach(obj.rounds, function(obj2){
+						var dbDate = new Date(obj2.interviewSchedule.interviewDateTime);
+						if(dbDate >= today && tomorrow >= dbDate){
+							showScheduleData.push({"cname":obj.candidateName, "round":obj2.interviewSchedule.roundName, "date":dbDate, "interviewId":obj.interviewerId});
+					    }
+					})
+				});
+				$scope.showScheduleData = showScheduleData;
+				if(data==undefined || data == null || data.length == 0){
+					$scope.hideNoInterviewMsg = false;
+				}
+			}).error(function(data, status, headers, config) {
+				$log.error(data);
+			})
+			
+		} else {
 			$scope.hideNoInterviewMsg = false;
 		}
-	}).catch(function(msg){
-		$log.error(msg);
-		$scope.hideNoInterviewMsg = false;
-	});
-	
+	}).error(function(data, status, headers, config) {
+		$log.error(data);
+	})
+
 	$scope.showInterview = function(obj, obj2) {
 		jobCodeService1.setjobCode(obj);
 		jobCodeService1.setinterviewRound(obj2);
