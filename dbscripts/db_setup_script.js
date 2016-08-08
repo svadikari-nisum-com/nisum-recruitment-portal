e57c1c22da9983b4fa6861eb984f5abd7f1da977
nbolla <nbolla@nisum.com>
2016-08-08 14:13:59 +0530

    Added FunctionalTeam in Info document(Mongo DB) script

--- dbscripts/db_setup_script.js
+++ dbscripts/db_setup_script.js
@@ -47,6 +47,8 @@ db.Info.insert({"_id":"Priority",value:["Low", "Medium", "High"]});
 db.Info.insert({"_id":"salary",value:[ "$6000 - $7000", "$7000 - $9000","$9000 - $10000", "$10000- $12000"]});
 db.Info.insert({"_id":"jobType",value:["Full Time", "Part Time"]});
 db.Info.insert({"_id":"status",value:["Selected", "Rejected", "Hold"]});
+db.Info.insert({"_id":"FunctionalTeam",value:["DEV", "QA", "SUPPORT","NOC"]});
+
 
 // DEVELOPER User data
 db.UserInfo.insert({"_id":"prawate@nisum.com",'name':"Pradeep Rawate","mobileNumber":"+918087810808","dob":"",location:"Pune",skypeId:"prawate",roles:["ROLE_ADMIN"],isNotAvailable:false,timeSlots:[{day:"Wednesday",time:"2015-06-17T09:35:24.899Z",hour:"2"},{day:"Friday",time:"2015-06-17T09:35:24.899Z",hour:"1"}]});
