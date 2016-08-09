5f9ba15fa01a068b53244b352c2dda872d1f6ab3
Shyam Vadikari <svadikari@nisum.com>
2016-08-09 17:51:41 +0530

    File download issue

--- src/main/java/com/nisum/employee/ref/controller/ProfileController.java
+++ src/main/java/com/nisum/employee/ref/controller/ProfileController.java
@@ -2,9 +2,10 @@ package com.nisum.employee.ref.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.mongodb.gridfs.GridFSDBFile;
-import com.nisum.employee.ref.domain.Profile;
 import com.nisum.employee.ref.service.IProfileService;
 import com.nisum.employee.ref.view.ProfileDTO;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -88,18 +86,16 @@ public class ProfileController {
 	@ResponseStatus(HttpStatus.OK)
 	@Secured({"ROLE_ADMIN","ROLE_USER","ROLE_HR","ROLE_RECRUITER","ROLE_MANAGER","ROLE_INTERVIEWER"})
 	@RequestMapping(value = "/fileDownload", method = RequestMethod.GET)
-	public ResponseEntity<HttpServletResponse> downloadOndemandOrder(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "candidateId", required = true) String candidateId) throws Exception {
+	public void downloadFile(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "candidateId", required = true) String candidateId) throws Exception {
 		List<GridFSDBFile> files = profileService.getFileData(candidateId);
 		GridFSDBFile file = files.get(0);
 		if (file != null) {
-			response.setContentType(file.getContentType().toString());
-			response.setContentLength((new Long(file.getLength()).intValue()));
+			response.setContentType(file.getContentType());
+			response.setContentLength((int) file.getLength());
 			IOUtils.copyLarge(file.getInputStream(), response.getOutputStream());
 		} else {
 			log.info("file does not exist");
 		}
-		return new ResponseEntity<HttpServletResponse>(response, HttpStatus.OK);
-	
 	}
 }
 
