5ee4bd7dee4140e4f7a83f81a8a1c3beeb108339
Nbolla <nbolla@nisum.com>
2016-09-21 22:05:40 +0530

    Added test cases for OfferService

--- src/test/java/com/nisum/employee/ref/service/OfferServiceTest.java
+++ src/test/java/com/nisum/employee/ref/service/OfferServiceTest.java
@@ -1,14 +1,25 @@
 package com.nisum.employee.ref.service;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nisum.employee.ref.converter.OfferConverter;
+import com.nisum.employee.ref.domain.Offer;
+import com.nisum.employee.ref.exception.ServiceException;
 import com.nisum.employee.ref.repository.OfferRepository;
 import com.nisum.employee.ref.view.OfferDTO;
+import com.nisum.employee.ref.view.PositionDTO;
+import com.nisum.employee.ref.view.UserInfoDTO;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OfferServiceTest {
 
 	@Mock
@@ -18,7 +29,7 @@ public class OfferServiceTest {
 	private OfferConverter offerConverter;
 
 	@Mock
-	private GenerateOfferService offerService;
+	private GenerateOfferService generateOfferService;
 	
 	@Mock
 	private UserService userService;
@@ -28,47 +39,93 @@ public class OfferServiceTest {
 	@Mock
 	private PositionService positionService;
 	
+	@InjectMocks
+	private OfferService offerService;
 	
 	@Test
-	public void testSaveOffer() {
+	public void testSaveOffer() throws ServiceException {
 		
+		PositionDTO position =  new PositionDTO();
+		position.setLocation("Hyd");
 		
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
+		userInfoDTO.setName("Naga");
 		
+		Mockito.when(offerConverter.convertToEntity(getOfferDTO())).thenReturn(getOffer());
+		Mockito.doNothing().when(offerRepository).saveOffer(getOffer());
+		Mockito.doNothing().when(offerRepository).updateInterviewDetails(getOffer());
+		Mockito.when(positionService.retrievePositionByJobCode(Mockito.anyString())).thenReturn(position);
+		Mockito.when(userService.retrieveUserByRoleAndLocation(Mockito.anyString(), Mockito.anyString())).thenReturn(Arrays.asList(userInfoDTO));
 		
-		
+		offerService.saveOffer(getOfferDTO());
 		
 	}
 
 	@Test
-	public void testGenerateNotification() {
+	public void testGenerateNotification() throws ServiceException {
+		
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
+		userInfoDTO.setName("Naga");
+		Mockito.when(userService.retrieveUserByName(Mockito.anyString())).thenReturn(Arrays.asList(userInfoDTO));
+		Mockito.doNothing().when(notificationService).sendOfferNotificationMail(Mockito.anyString(), Mockito.anyString(),Mockito.anyString(), Mockito.anyString());
+		offerService.generateNotification("Naga","Naga","Naga");
 	}
 
 	@Test
-	public void testGenerateOfferLetter() {
+	public void testGenerateOfferLetter() throws ServiceException {
+		
+		Mockito.doNothing().when(generateOfferService).generateOffer(getOfferDTO());
+		offerService.generateOfferLetter(getOfferDTO());
 	}
 
-	@Test
+	/*@Ignore
 	public void testSaveResumeInBucket() {
-	}
+		
+		Mockito.doNothing().when(offerRepository).saveResumeInBucket(Mockito.anyObject(),Mockito.anyString());
+		offerService.saveResumeInBucket((MultipartFile) new Object(), "Naga");
+	}*/
 
 	@Test
 	public void testGetNextStatuses() {
+		
+		
+		offerService.getNextStatuses("Initiated");
 	}
 
 	@Test
 	public void testGetOffers() {
+		
+		Mockito.when(offerRepository.getOffers()).thenReturn(Arrays.asList(getOffer()));
+		Mockito.when(offerConverter.convertToDTOs(Arrays.asList(getOffer()))).thenReturn(Arrays.asList(getOfferDTO()));
+		offerService.getOffers();
+		
 	}
 
 	@Test
 	public void testGetOffer() {
+		
+		Mockito.when(offerConverter.convertToDTO(getOffer())).thenReturn(getOfferDTO());
+		offerService.getOffer("nbolla@nisum.com");
 	}
 
 	@Test
 	public void testGetOffersByJobcode() {
+		
+		Mockito.when(offerRepository.getOffersByJobcode("Naga")).thenReturn(Arrays.asList(getOffer()));
+		Mockito.when(offerConverter.convertToDTOs(Arrays.asList(getOffer()))).thenReturn(Arrays.asList(getOfferDTO()));
+		offerService.getOffersByJobcode("Naga");
 	}
 
 	@Test
 	public void testGetOffersByManagerId() {
+		
+		PositionDTO position = new PositionDTO();
+		position.setJobcode("Naga");
+		Mockito.when(positionService.retrieveAllPositions()).thenReturn(Arrays.asList(position));
+		Mockito.when(offerRepository.getOffersByJobcode("Naga")).thenReturn(Arrays.asList(getOffer()));
+		Mockito.when(offerConverter.convertToDTOs(Arrays.asList(getOffer()))).thenReturn(Arrays.asList(getOfferDTO()));
+		offerService.getOffersByManagerId("Naga");
+		
 	}
 
 	private OfferDTO getOfferDTO()
@@ -76,8 +133,20 @@ public class OfferServiceTest {
 		OfferDTO offerDTO = new OfferDTO();
 		
 		offerDTO.setCandidateName("Naga");
-		
+		offerDTO.setStatus("Initiated");
+		offerDTO.setJobcodeProfile("Ram");
+		offerDTO.setReportingManager("Bolla");
 		
 		return offerDTO;
+	}
+	private Offer getOffer()
+	{
+		Offer offer = new Offer();
+		offer.setCandidateName("Naga");
+		offer.setStatus("Initiated");
+		offer.setJobcodeProfile("Ram");
+		offer.setReportingManager("Bolla");
+		
+		return offer;
 	}
 }
