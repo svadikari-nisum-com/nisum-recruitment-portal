4f591b9e369fdac53b4d0188b43c2a74fa64ee91
Durga Prasad Narikalapa <dprasad@nisum.com>
2016-09-26 21:17:06 +0530

    UI Restructure -> javascript bower integration and version upgrade updated

--- src/main/webapp/js/recruitment/recruitment.js
+++
@@ -1,9 +0,0 @@
-app.controller("recruitmentCtrl", ['$scope', '$http', '$filter', '$timeout','$q','$state', '$location', 
-    function($scope, $http, $filter, $timeout, $q, $state, $location) {
-
-	$scope.isActive = function (stateName) {
-		$scope.indeX = $state.is(stateName);
-        return $scope.indeX;
-    };
-
-}]);
